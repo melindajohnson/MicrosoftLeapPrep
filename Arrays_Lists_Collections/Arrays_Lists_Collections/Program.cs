8c928e87688d1c5a323aa51f38530ee48e10534f
Melinda <melistajohn@gmail.com>
2020-12-30 16:57:14 -0800

    lists intro

--- Arrays_Lists_Collections/Arrays_Lists_Collections/Program.cs
+++ Arrays_Lists_Collections/Arrays_Lists_Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Arrays_Lists_Collections
 {
@@ -6,7 +7,61 @@ namespace Arrays_Lists_Collections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+           // workingWithStrings();
+            workingWithotherTypes();
+
+        }
+        public static void workingWithotherTypes()
+        {
+            var fibonacciNumbers = new List<int> { 1, 1 };
+
+            while(fibonacciNumbers.Count < 20)
+            {
+                var previous = fibonacciNumbers[fibonacciNumbers.Count - 1];
+                var previous2 = fibonacciNumbers[fibonacciNumbers.Count - 2];
+
+                fibonacciNumbers.Add(previous + previous2);
+            }
+            
+
+            foreach (var item in fibonacciNumbers)
+                Console.WriteLine(item);
+
+        }
+            public static void workingWithStrings()
+        {
+            //lists and arrays
+            var names = new List<string> { "<name>", "Ana", "Felipe" };
+            names.Add("Maria");
+            names.Add("Bill");
+            names.Remove("Ana");
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
+
+
+            Console.WriteLine($"Hello {names[1].ToUpper()}!");
+            Console.WriteLine($"The list has {names.Count} people in it");
+
+
+            //search 
+
+            var index = names.IndexOf("Maria");
+            if (index != -1)
+                Console.WriteLine($"The name {names[index]} is at index {index}");
+
+            var notFound = names.IndexOf("Not Found");
+            Console.WriteLine($"When an item is not found, IndexOf returns {notFound}");
+
+
+            //sort
+            names.Sort(); // in place sorting
+            foreach (string name in names)
+            {
+                Console.WriteLine($"Hello {name.ToUpper()}!");
+            }
         }
     }
 }
--- Branches_Loops/Branches_Loops/Program.cs
+++ Branches_Loops/Branches_Loops/Program.cs
@@ -5,7 +5,9 @@ namespace Branches_Loops //used to group and organize code (own package)
     class Program
     {
         static void Main(string[] args) //first method -> main method which is the entry point
-        { 
+        {
+
+            //Branches example
             int a = 5;
             int b = 3;
             int c = 4;
@@ -19,6 +21,40 @@ namespace Branches_Loops //used to group and organize code (own package)
                 Console.WriteLine("The answer is not greater than 10");
                 Console.WriteLine("Or the first number is not equal to the second");
             }
+
+            //Loop example using while
+            int counter1 = 0;
+            while (counter1 < 10)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter1}");
+                counter1++;
+            }
+
+
+            //Loop example using do while
+            int counter2 = 10;
+            do
+            {
+                Console.WriteLine($"Hello World! The counter is {counter2}");
+                counter2++;
+            } while (counter2 < 10);
+
+
+            //Loop example using for
+            //initializer           condition       iterator
+            for (int counter3 = 0; counter3 < 10; counter3++)
+            {
+                Console.WriteLine($"Hello World! The counter is {counter3}");
+            }
+
+
+            // combine branches and loops using fors and if statements.
+            int sum = 0;
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 3 == 0) sum += i;
+            }
+            Console.WriteLine(sum);
         }
     }
 }
